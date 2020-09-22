94f02ae6e9771bdc9dfa00c85ba9d1652f4bffea
ishaarora01 <ishaarora.87@gmail.com>
2020-09-21 20:54:40 -0400

    added type field to pokemon object

--- pokemon-app/models/pokemon.js
+++ pokemon-app/models/pokemon.js
@@ -1,31 +1,38 @@
 const pokemon = [
     {
       name: "Bulbasaur",
-      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
+      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg",
+      type: "fire"
     },
     {
       name: "Ivysaur",
-      img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
+      img: "http://img.pokemondb.net/artwork/ivysaur.jpg",
+      type: "fire"
     },
     {
       name: "Venusaur",
-      img: "http://img.pokemondb.net/artwork/venusaur.jpg"
+      img: "http://img.pokemondb.net/artwork/venusaur.jpg",
+      type: "water"
     },
     {
       name: "Charmander",
-      img: "http://img.pokemondb.net/artwork/charmander.jpg"
+      img: "http://img.pokemondb.net/artwork/charmander.jpg",
+      type: "water"
     },
     {
       name: "Charizard",
-      img: "http://img.pokemondb.net/artwork/charizard.jpg"
+      img: "http://img.pokemondb.net/artwork/charizard.jpg",
+      type: "grass"
     },
     {
       name: "Squirtle",
-      img: "http://img.pokemondb.net/artwork/squirtle.jpg"
+      img: "http://img.pokemondb.net/artwork/squirtle.jpg",
+      type: "grass"
     },
     {
       name: "Wartortle",
-      img: "http://img.pokemondb.net/artwork/wartortle.jpg"
+      img: "http://img.pokemondb.net/artwork/wartortle.jpg",
+      type: "grass"
     }
 ];
 
