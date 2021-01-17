5fc49a93964eefd03b71fdffff3dd6395d527fe9
ericb2020 <ebrauer83@gmail.com>
2021-01-17 14:04:32 -0600

    Commit 4: index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const app = express ();
+//const methodOverride = require("method-override");
+const pokemon = require("./models/pokemon.js");
 
-const app = express();
 
 
 
+//index route
+app.get("/pokemon/", (req, res) => {
+    res.render("index.ejs", {
+        pokemon: pokemon,
 
-app.get("/pokemon/", (req,res) => {
-    res.send(pokemon);
-})
-
-app.get("/pokemon/:index", (req,res) => {
-    res.send(pokemon[req.params.index]);
+    });
 });
+
+// app.get("/pokemon/", (req,res) => {
+//     res.send(pokemon);
+// })
+
+// app.get("/pokemon/:index", (req,res) => {
+//     res.send(pokemon[req.params.index]);
+// });
 
 
 app.listen(3000, () => {
