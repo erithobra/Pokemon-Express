e35140281bd913668d28b2e75cc0fd3f969cacf8
ericb2020 <ebrauer83@gmail.com>
2021-01-17 18:02:50 -0600

    Commit 7: full CRUD functionality

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express ();
-//const methodOverride = require("method-override");
+const methodOverride = require("method-override");
 const pokemon = require("./models/pokemon.js");
+
+//middleware
+app.use((req, res, next) => {
+    console.log("I run for all routes");
+    next();
+});
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+
 
 //index route - Commit 4
 app.get("/pokemon/", (req, res) => {
@@ -9,6 +18,41 @@ app.get("/pokemon/", (req, res) => {
         pokemon: pokemon,
 
     });
+});
+
+//create route - Commit 7
+
+//new show for new.ejs
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+//create route (POST)
+app.post("/pokemon/", (req,res) => {
+    pokemon.push(req.body);
+    console.log(pokemon);
+    res.redirect("/pokemon/");
+});
+
+//edit route
+app.get("/pokemon/:index/edit", (req, res) => {
+    res.render("edit.ejs", {
+        poke: pokemon[req.params.index],
+        index: req.params.index
+    })
+})
+
+//put route
+app.put("/pokemon/:index", (req, res) => {
+let index = parseInt(req.params.index);
+pokemon[index] = req.body
+res.redirect("/pokemon/");
+});
+
+//delete route
+app.delete("/pokemon/:index", (req,res) => {
+    pokemon.splice(req.params.index, 1);
+    res.redirect("/pokemon/");
 });
 
 //show route - Commit 5
